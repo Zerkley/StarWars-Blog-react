18173d67120437443d3c2e5e9b66a22de9d356be
Zerkley <miguelmr94@gmail.com>
2023-07-10 12:05:46 +0100

    V1 structure made, pending fetch

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,41 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+  return {
+    store: {
+      peopleGeneral: [],
+      people: [],
+      planets: [],
+    },
+    actions: {
+      // Use getActions to call a function within a fuction
+      getAllPeople: () => {
+        fetch("https://www.swapi.tech/api/people")
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            setStore({ peopleGeneral: data.results });
+          })
+          .catch((err) => console.error(err));
+      },
+      getPeople: (id) => {
+        fetch("https://www.swapi.tech/api/people/" + id)
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            setStore({ people: data.result });
+          })
+          .catch((err) => console.error(err));
+      },
+      getPlanets: () => {
+        fetch("https://www.swapi.tech/api/planets/1")
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            setStore({ planets: data.results });
+          })
+          .catch((err) => console.error(err));
+      },
+    },
+  };
 };
 
 export default getState;
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,50 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { CharacterCard } from "../component/characterCard";
+import { PlanetCard } from "../component/planetCard";
+import { Context } from "../store/appContext";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+  const { store, actions } = useContext(Context);
+  const people = store.people;
+  useEffect(() => {
+    actions.getAllPeople();
+  }, []);
+  return (
+    <div>
+      <div className="container-fluid">
+        <div className="row flex-row flex-nowrap overflow-auto">
+          <CharacterCard
+            id={peopleGeneral?.uid}
+            characterName={people.properties?.name}
+            gender={people.properties?.gender}
+            hairColor={people.properties?.hair_color}
+            eyeColor={people.properties?.eye_color}
+          />
+
+          <CharacterCard />
+          <CharacterCard />
+          <CharacterCard />
+          <CharacterCard />
+          <CharacterCard />
+          <CharacterCard />
+          <CharacterCard />
+        </div>
+      </div>
+      <div className="container-fluid">
+        <div className="row flex-row flex-nowrap overflow-auto">
+          <PlanetCard />
+          <PlanetCard />
+          <PlanetCard />
+          <PlanetCard />
+          <PlanetCard />
+          <PlanetCard />
+          <PlanetCard />
+          <PlanetCard />
+          <PlanetCard />
+        </div>
+      </div>
+    </div>
+  );
+};
